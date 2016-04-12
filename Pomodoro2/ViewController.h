e909fa2db262f12003648df771e95f636eac8b6b
Binh Nguyen <binhnguyen7968@gmail.com>
2016-04-12 21:13:52 +0700

    Initial Commit

---
+++ Pomodoro2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pomodoro2
+//
+//  Created by Nguyen Binh on 4/12/16.
+//  Copyright © 2016 Nguyen Binh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Pomodoro2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Pomodoro2
+//
+//  Created by Nguyen Binh on 4/12/16.
+//  Copyright © 2016 Nguyen Binh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
